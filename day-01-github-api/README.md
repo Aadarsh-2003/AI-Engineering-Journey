2faf69f3a74ef77c358c9dc1f968c3a29f8b96dd
Aadarsh-2003 <avi.sharma.2853@gmail.com>
2026-08-24 16:56:14 +0530

    start AI Engineering journey

---
+++ day-01-github-api/README.md
@@ -0,0 +1,3 @@
+# GitHub Profile Finder
+
+A beginner Python project demonstrating how to consume a public REST API.
---
+++ day-01-github-api/main.py
@@ -0,0 +1,31 @@
+import requests
+
+username = input("Enter GitHub username: ")
+
+url = f"https://api.github.com/users/{username}"
+
+response = requests.get(url)
+
+print(f"Status Code: {response.status_code}")
+
+if response.status_code == 200:
+    data = response.json()
+
+    print("\nThis code is in a separate branch, Profile finder !\n")
+
+    print("\n GitHub Profile")
+    print("====================")
+    print("Username:", data["login"])
+    print("Name:", data["name"] if data["name"] else "Not Available")
+    print("Public repositories:", data["public_repos"])
+    print("repo count check:", "This user has a large number of public repositories!" if data["public_repos"] > 50 else "This user has fewer than 50 public repositories.")
+    print("Account created at:", data["created_at"])
+    print("Followers:", data["followers"])
+    print("Following:", data["following"])
+    print("Location:", data["location"])
+    print("Profile:", data["html_url"])
+
+    # print(data)
+
+else:
+    print("user not found.")
